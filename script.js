d7ed4821f7abd39fd3bae94bfb013dc7d6dd1189
Top-Developer <top_dev@outlook.com>
2017-05-26 19:40:55 +0200

    implemented treemap chart

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var project = {
 	edges : [],
 	layers : [],
 	layer_count : -1,
+	layer_inout : [],
 	selectedNodeId : ''
 };
 
@@ -28,6 +29,7 @@ $(document).ready(function(){
 					project.edges = edgesReader(data);console.log(project.edges);
 					project.layer_count++;
 					project.layers.push({'nodes':project.nodes, 'edges':project.edges});console.log(project.layers);
+					project.layer_inout.push(-1);
 
 					network_display(d3.select('svg#main-svg'), project.nodes, project.edges);
 				}
@@ -64,27 +66,165 @@ function closePopup(){
 	project.layer_count = 0;
 }
 
-function treeMapDisplay(data){
+function treeMapDisplay(){
+
+	var sr = [], pr = [], sp  = [], fp = [], total = [];
+	total['sr'] = 0;
+	total['pr'] = 0;
+	total['sp'] = 0;
+	total['fp'] = 0;
+
+	console.log(project.layers[project.layer_count]['edges']);
+	console.log(project.layers[project.layer_count]['nodes']);
+
+	project.layers[project.layer_count]['edges'].forEach(function(e){
+
+		var theNode;
+
+		project.layers[project.layer_count]['nodes'].forEach(function(n){
+			if ( project.layer_inout[project.layer_count] == 1 ){
+				if( n['id'] == e['source']['id'] ) theNode = n;
+			}else if ( project.layer_inout[project.layer_count] == 0 ){
+				if( n['id'] == e['target']['id'] ) theNode = n;
+			}
+		});
+
+
+		if( theNode['type'] == 'SR') {
+			sr.push({
+				'label' : theNode['id'],
+				'value' : e['cost'],
+				'svalue' : parseInt( theNode['var'] )
+			});
+			total['sr'] += e['cost'];console.log(total['sr']);
+		}
+		if( theNode['type'] == 'PR') {
+			pr.push({
+				'label' : theNode['id'],
+				'value' : e['cost'],
+				'svalue' : parseInt( theNode['var'] )
+			});
+			total['pr'] += e['cost'];console.log(total['pr']);
+		}
+		if( theNode['type'] == 'SP') {
+			sp.push({
+				'label' : theNode['id'],
+				'value' : e['cost'],
+				'svalue' : parseInt( theNode['var'] )
+			});
+			total['sp'] += Math.round(e['cost']);console.log(total['sp']);
+		}
+		if( theNode['type'] == 'FP') {
+			fp.push({
+				'label' : theNode['id'],
+				'value' : e['cost'],
+				'svalue' : parseInt( theNode['var'] )
+			});
+			total['fp'] += e['cost'];console.log(total['fp']);
+		}
+	});
+
+	console.log(sr);console.log('\n');
+	console.log(pr);console.log('\n');
+	console.log(sp);console.log('\n');
+	console.log(fp);console.log('\n');
+
+	total['all'] = total['sr'] + total['pr'] + total['sp'] + total['fp'];	console.log(total);
+
+	var data = [
+		{
+			'label' : 'Focused Unit',
+			'fillcolor' : '8c8c8c',
+			'value' : total['all']
+		}
+	];
+	data[0]['data'] = [];
+	if ( total['sr'] != 0 ){
+		data[0]['data'].push({
+			'label' : 'Support Resource',
+			'value' : total['sr'],
+			'data' : sr
+		});
+	}
+	if ( total['pr'] != 0 ){
+		data[0]['data'].push({
+			'label' : 'Production Resource',
+			'value' : total['pr'],
+			'data' : pr
+		});
+	}
+	if ( total['sp'] != 0 ){
+		data[0]['data'].push({
+			'label' : 'Semi-finished Product',
+			'value' : total['sp'],
+			'data' :sp
+		});
+	}
+	if ( total['fp'] != 0 ){
+		data[0]['data'].push({
+			'label' : 'Finished Product',
+			'value' : total['fp'],
+			'data' : fp
+		});
+	}
 
 	FusionCharts.ready(function(){
-		D3.select('#chartContainer').style('display', 'block');
+		d3.select('#chartContainer').style('display', 'block');
     var revenueChart = new FusionCharts({
-        "type": "column2d",
-        "renderAt": "chartContainer",
-        "width": "500",
-        "height": "300",
-        "dataFormat": "json",
-        "dataSource":  {
-          "chart": {
-            "caption": "Monthly revenue for last year",
-            "subCaption": "Harry's SuperMart",
-            "xAxisName": "Month",
-            "yAxisName": "Revenues (In USD)",
-            "theme": "fint"
-					},
-         "data":data
-			 }
+			type: 'treemap',
+      renderAt: 'chartContainer',
+			width: '550',
+			height: '350',
+			dataFormat: 'json',
+			dataSource: {
+				"chart" : {
+		        "animation": "0",
+		        "hidetitle": "1",
+		        "plottooltext": "$label Sale: $$value Growth: $svalue%",
+		        "spacex": "0",
+		        "spacey": "0",
+		        "horizontalpadding": "1",
+		        "verticalpadding": "1",
+		        "hoveronallsiblingleaves": "1",
+		        "plotborderthickness": ".5",
+		        "plotbordercolor": "666666",
+		        "legendpadding": "0",
+		        "legenditemfontsize": "10",
+		        "legenditemfontbold": "1",
+		        "showlegend": "1",
+		        "legenditemfontcolor": "3d5c5c",
+		        "algorithm": "squarified",
+		        "caption": "Harry's SuperMart: Sales Team Performance Analysis",
+		        "legendscalelinethickness": "0",
+		        "legendcaptionfontsize": "10",
+		        "subcaption": "Year Till Date",
+		        "legendcaption": "Growth in sales - Compared to previous year",
+		        "theme": "zune"
+		    },
+	    	"data" : data,
+				"colorrange": {
+	        "mapbypercent": "1",
+	        "gradient": "1",
+	        "minvalue": "-25",
+	        "code": "e24b1a",
+	        "startlabel": "Decline",
+	        "endlabel": "Rise",
+	        "color": [
+						{
+							"code": "ffffff",
+	            "maxvalue": "0",
+	            "label": "Static"
+						},
+						{
+							"code": "6da81e",
+	            "maxvalue": "100",
+	            "label": "AVERAGE"
+						}
+	        ]
+				}
+			}
 		});
+		console.log(data);
 		revenueChart.render();
 	})
 }
@@ -120,7 +260,7 @@ function consumptionInOutbound(inout, d){
 												'<div class = "col-6">' + e['source']['id'] + '</div>' +
 												'<div class = "col-3">' + e['cost'] + '</div>' +
 												'<div class = "col-3">' + e['quantity'] + '</div>' +
-											'</div>'
+											'</div>';
 			}
 		}else if ( inout == 0 ){
 			if( e['source']['id'] == d ){
@@ -159,6 +299,7 @@ function consumptionInOutbound(inout, d){
 		'nodes':fNodes,
 		'edges':fEdges
 	});
+	project.layer_inout.push(inout);
 
 	if( project.layer_count == 1){
 		d3.select('#sub-svg')
@@ -172,6 +313,7 @@ function consumptionInOutbound(inout, d){
 			d3.selectAll('svg#layer-svg > *').remove();
 			while(project.layer_count > 1){
 				project.layers.pop();
+				project.layer_inout.pop();
 				d3.select('[index="'+ project.layer_count +'"]').remove();
 				project.layer_count--;
 			}
@@ -195,6 +337,7 @@ function consumptionInOutbound(inout, d){
 			d3.selectAll('svg#layer-svg > *').remove();
 			while( project.layer_count > +d3.select(this).attr('index') ){
 				project.layers.pop();
+				project.layer_inout.pop();
 				d3.select('[index="'+ project.layer_count +'"]').remove();
 				project.layer_count--;
 			}
